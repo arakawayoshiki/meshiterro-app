2752a718591618134407b9f53e38148e81173fc1
arakawayoshiki <yamato4tennis@gmail.com>
2021-07-11 09:23:04 +0000

    [finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '909f14145f2c0aaf3fa5011c7c39111eafef4e08699d8b613629d809c4e7f8be7855a11e743bb2bbe1b5e18f1eb90fcc7f11a6c6b13c8e3b0c61b9e85d5d3787'
